36d1b8bc9f77568d500db467e372c9657297e3a2
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2022-10-15 03:33:25 +0530

    resolved linter errors

--- client/pages/search.js
+++ client/pages/search.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import { Box } from '@mui/system'
 
-const search = () => {
+const Search = () => {
     const { query } = useRouter().query
   return (
     <Box sx={{height: 300, mt: '60px'}}>{query}</Box>
   )
 }
 
-export default search
+export default Search
