37fb758ac76a63a74503fda4d03eefbbe89e81ff
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2022-10-15 21:03:16 +0530

    updated readme for the project

--- client/comps/Sections/Categories.js
+++ client/comps/Sections/Categories.js
@@ -32,55 +32,55 @@ const categories = [
 ];
 
 const tags = [
-    {
-        title: 'Javascript',
-        path: '/search?query=javascript'
-    },
-    {
-        title: 'CSS',
-        path: '/search?query=css'
-    },
-    {
-        title: 'HTML',
-        path: '/search?query=html'
-    },
-    {
-        title: 'ReactJS',
-        path: '/search?query=react'
-    },
-    {
-        title: 'AngularJS',
-        path: '/search?query=angular'
-    }
+  {
+    title: 'Javascript',
+    path: '/search?query=javascript'
+  },
+  {
+    title: 'CSS',
+    path: '/search?query=css'
+  },
+  {
+    title: 'HTML',
+    path: '/search?query=html'
+  },
+  {
+    title: 'ReactJS',
+    path: '/search?query=react'
+  },
+  {
+    title: 'AngularJS',
+    path: '/search?query=angular'
+  }
 ]
 
 
 const Categories = () => {
   return (
-    <Box sx={{mb: 4}}>
+    <Box sx={{ mb: 4 }}>
       <SubHeading icon={<></>} title={'Categories'} />
       <Stack direction="column">
         {categories.map((item) => (
-        <>
-          <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', '&:hover': {bgcolor: 'lightgray'}}}>
-            <Link href={item.path}>
-            <a>
-                <Typography variant='body1' sx={{pt: 1.5, pb: 1.5, pl: 1}}>{item.title}</Typography>
-            </a>
-            </Link>
-            <Paper sx={{fontSize: 14,p: 1,display: 'flex', alignItems: 'center', bgcolor: deepPurple[500], color: 'white', height: 'fit-content'}} elevation={0}>{item.total}</Paper>
-          </Box>
-          <Divider />
-        </>
+          <>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', '&:hover': { bgcolor: 'lightgray' } }}>
+              <Link href={item.path}>
+                <a>
+                  <Typography variant='body1' sx={{ pt: 1.5, pb: 1.5, pl: 1 }}>{item.title}</Typography>
+                </a>
+              </Link>
+              <Paper sx={{ fontSize: 14, p: 1, display: 'flex', alignItems: 'center', bgcolor: deepPurple[500], color: 'white', height: 'fit-content' }} elevation={0}>{item.total}</Paper>
+            </Box>
+            <Divider />
+          </>
         ))}
       </Stack>
-      <Box sx={{mt: 5}}>
+      <Box sx={{ mt: 5 }}>
         {tags.map((item) => (
           <Link href={item.path}>
-          <a>
-            <Button variant='contained' sx={{fontSize: 14, width: 'fit-content', m: 1}}>{item.title}</Button>
+            <a>
+              <Button variant='contained' sx={{ fontSize: 14, width: 'fit-content', m: 1 }}>{item.title}</Button>
             </a>
-            </Link>
+          </Link>
         ))}
       </Box>
     </Box>
